c5ba43dc5543b70bb5b067e6c17af317208ea854
dlfinis <dln.finis@gmail.com>
2016-05-26 00:09:46 -0500

    Fix static path

--- app/middlewares.js
+++ app/middlewares.js
@@ -6,7 +6,7 @@ var config = require('../config/app');
 module.exports = function(app) {
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
-  app.use(express.static(config.staticPath));
+  app.use(config.staticPath, express.static(path.resolve(__dirname, '../public')));
 	app.get(config.staticPath + '/*', function(req, res) {
 		res.status(404).send('Not found');
 	});
