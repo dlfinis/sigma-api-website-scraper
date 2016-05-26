3b66636744ad56a60426b9cadd412c22cc236696
dlfinis <dln.finis@gmail.com>
2016-05-25 23:25:37 -0500

    Add gitkeep

--- config/files.js
+++
@@ -1,7 +0,0 @@
-var path = require('path');
-
-module.exports = {
-	directory: path.resolve(__dirname, '../public/files/'),
-	previewPath: '/static/files/{directory}',
-	downloadPath: '/sites/{directory}/download'
-};
