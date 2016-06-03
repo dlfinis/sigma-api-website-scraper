e4201f065df1a49592bd9d49b1c773964fff7044
dlfinis <dln.finis@gmail.com>
2016-06-03 14:45:01 -0500

    Change port

--- app/index.js
+++ app/index.js
@@ -10,7 +10,8 @@ var app = express();
 addMiddlewares(app);
 addRoutes(app);
 
-app.listen(config.port,config.ipaddress, function () {
-  console.log("Listening on " + config.ipaddress + ", server_port " + config.port );
-  console.log("Api Scraper Init");
+app.listen(config.port, function () {
+  console.log("Listening on " + process.env.OPENSHIFT_NODEJS_IP || config.ipaddress +
+    ", server_port " + config.port );
+  console.log("+ Api Scraper Init");
 });
--- config/app.js
+++ config/app.js
@@ -1,5 +1,5 @@
 module.exports = {
   ipaddress: '127.0.0.1',
-	port: 3000,
+	port: 3100,
 	staticPath: '/static'
 };
