8daaabd9e7dcb88759f82642a05f3c34fe790b59
dlfinis <dln.finis@gmail.com>
2016-06-03 12:30:08 -0500

    Add OpenShift Varaibles

--- app/index.js
+++ app/index.js
@@ -6,8 +6,14 @@ var addRoutes = require('./routes');
 var config = require('./../config/app');
 var app = express();
 
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
 
 addMiddlewares(app);
 addRoutes(app);
 
-app.listen(config.port);
+app.listen(port,ipaddress, function () {
+  console.log("Listening on " + server_ip_address + ", server_port " + port );
+  console.log("Api Scraper Init");
+});
