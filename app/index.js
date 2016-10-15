93b99450f1c017abd9f97b37b4e8e960b06a8c82
dlfinis <dln.finis@gmail.com>
2016-10-15 16:00:17 -0500

    Add env variables

--- app/index.js
+++ app/index.js
@@ -11,6 +11,6 @@ addMiddlewares(app);
 addRoutes(app);
 
 app.listen(config.port, function () {
-  console.log("Listening on " + (process.env.OPENSHIFT_NODEJS_IP || config.ipaddress) + ", server_port " + config.port );
-  console.log("+ Api Scraper Init");
+  console.log("Listening on " + (process.env.OPENSHIFT_NODEJS_IP || process.env.NODE_PORT config.ipaddress) + ", server_port " + process.env.SCRAPER_PORT || config.port );
+  console.log("+ API Scraper Init");
 });
