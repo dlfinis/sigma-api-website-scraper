b45ea24065f58e3d22d0ca9c7e1dbe4dc9c61cf7
dlfinis <dln.finis@gmail.com>
2016-10-25 03:48:56 -0500

    Update options of generation of name based in url

--- core/sites.js
+++ core/sites.js
@@ -25,9 +25,9 @@ function getDiffDays ( date1, date2 ) {
 function getSiteRawDirname (siteUrl) {
 	var urlObj = url.parse(siteUrl);
 	var domain = urlObj.host;
-	var path = urlObj.path.replace(new RegExp('/', 'g'), '_');
-	path = urlObj.path.replace(new RegExp('?', 'g'), '-9');
-	path = urlObj.path.replace(new RegExp('&', 'g'), '-8');
+	var path = urlObj.path.replace(new RegExp('/', 'g'), '_7-_');
+  path = urlObj.path.replace(new RegExp('&', 'g'), '_8-_');
+	path = urlObj.path.replace(new RegExp('?', 'g'), '_9-_');
 	return {
 		domain : domain,
 		path : path,
